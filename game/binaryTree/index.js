47a36b44a6626e5e57ff47e957f15e722fde6b0d
Midoria7 <buptxmh@bupt.edu.cn>
2023-10-04 22:18:45 +0800

    Fix the index.js of some problems

--- game/binaryTree/index.js
+++ game/binaryTree/index.js
@@ -3,7 +3,6 @@ module.exports = {
     pid: 'binaryTree',
     description_file: 'problem.md',
     points: 50, // 本题的分数
-    gameover: true,
     checker(ans) {
         return ans === 'ABDECF';
     }
--- game/rollDice/index.js
+++ game/rollDice/index.js
@@ -3,7 +3,6 @@ module.exports = {
     pid: 'rollDice',
     description_file: 'problem.md',
     points: 50, // 本题的分数
-    gameover: true,
     checker(ans) {
         return ans == 35;
     }
