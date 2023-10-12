14e37d84854f9c5fca0ac3a5dbe9d2f4f7723cc1
YouXam <youxam@outlook.com>
2023-10-12 19:55:34 +0800

    update assessment cheker

--- game/Assessment/index.js
+++ game/Assessment/index.js
@@ -6,7 +6,46 @@ module.exports = {
     points: 80,
     interval: 10 * 1000,
     checker: async (ans, ctx) => {
-        
+        const testData = Array.from({ length: 10 }, () => Math.random());
+        const code = `
+        #include <cstdio>
+    
+        const int maxn = 10 + 5;
+        int n;
+        double f[maxn], g[maxn], h[maxn];
+    
+        int main() {
+            n = 10;
+            double p[] = {0, ${testData.join(', ')}};
+            for (int i = 1; i <= n; i++) {
+                f[i] = (f[i - 1] + 1.0) * p[i];
+                g[i] = (g[i - 1] + f[i - 1] * 2.0 + 1.0) * p[i];
+                ${ans};
+            }
+            printf("%lf", h[n]);
+            return 0;
+        }`;
+    
+        const res = await ctx.runCode(code, 'cpp');
+        if (res.code) {
+            ctx.msg("Error:" + res.stdout);
+            return false;
+        }
+        const stdout = parseFloat(res.stdout);
+        let f = new Array(15).fill(0);
+        let g = new Array(15).fill(0);
+        let h = new Array(15).fill(0);
+        let p = [0, ...testData];
+        for (let i = 1; i <= 10; i++) {
+            f[i] = (f[i - 1] + 1.0) * p[i];
+            g[i] = (g[i - 1] + f[i - 1] * 2.0 + 1.0) * p[i];
+            h[i] = h[i - 1] + (g[i - 1] * 3 + f[i - 1] * 3 + 1) * p[i];
+        }
+        const expected = h[10];
+        if (stdout[i] != expected) {
+            ctx.msg("Wrong Answer");
+            return false;
+        }
         return true;
     },
     next: [
