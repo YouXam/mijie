b56c50bdb36082d748c92406f0e01c181e3ed383
YouXam <youxam@outlook.com>
2024-10-11 08:56:48 +0800

    Rename
    Signed-off-by: YouXam <youxam@outlook.com>

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -117,7 +117,7 @@ router.beforeEach((to, from, next) => {
         next({ path: '/404', replace: true,  })
         return
     }
-    document.title = to.meta.title ? to.meta.title + ' | 哈士奇再现' : '哈士奇再现'
+    document.title = to.meta.title ? to.meta.title + ' | 哈士奇的复仇' : '哈士奇的复仇'
     NProgress.start()
     next();
 });
