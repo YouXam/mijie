c956929d796cac26724de282b496dbb48d685ec5
YouXam <youxam@outlook.com>
2025-01-21 01:14:00 +0800

    Fix BesiegeWithoutAssault storage bug
    Signed-off-by: YouXam <youxam@outlook.com>

--- game/BesiegeWithoutAssault/index.ts
+++ game/BesiegeWithoutAssault/index.ts
@@ -20,10 +20,13 @@ export default {
             if (isNaN(data.x) || isNaN(data.y)) return
             ctx.gameStorage.set('player', { x: data.x, y: data.y });
             ctx.gameStorage.set('enemy', { x: 0, y: 0 });
+            ctx.gameStorage.delete('end')
         })
         app.on<{ x: number, y: number }>('move', async (data, ctx) => {
             const player = ctx.gameStorage.get<{ x: number, y: number }>('player');
             const enemy = ctx.gameStorage.get<{ x: number, y: number }>('enemy');
+            const end = ctx.gameStorage.get<boolean>('end')
+            if (end) return
             if (!player || !enemy || isNaN(data.x) || isNaN(data.y)) return;
             const result = await move(
                 new Coordinate(player.x, player.y),
@@ -32,8 +35,13 @@ export default {
             )
             if (result.result === 'success') {
                 ctx.pass(`敌军累计路程：${result.length}`)
+                ctx.gameStorage.set('end', true)
             } else if (result.result !== 'continue') {
                 ctx.nopass(result.result === 'out-of-range' ? '敌军逃出了包围圈' : '距离敌军太近了')
+                ctx.gameStorage.set('end', true)
+            } else {
+                ctx.gameStorage.set('player', { x: result.you.x, y: result.you.y });
+                ctx.gameStorage.set('enemy', { x: result.enemy.x, y: result.enemy.y });
             }
         })
     }
